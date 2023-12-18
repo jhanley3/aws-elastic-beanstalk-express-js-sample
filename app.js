0ef5a840c00c9b4012eb1ceed1dfa192c3c52080
jhanley3 <jayde.hanley@empnet.ca>
2023-12-18 13:07:22 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This line was changed.'));
+app.get('/', (req, res) => res.send('Testing review stage of CodePipeline.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
